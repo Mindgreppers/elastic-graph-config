b56dae03dc97b57c1a7ba806f5841bc682f52900
Ayush Ghai <ayush.ghai@mindgrep.com>
2021-01-25 17:12:46 +0530

    relationship.txt and migration file fix

--- data-pipelines/migration.js
+++ data-pipelines/migration.js
@@ -7,8 +7,8 @@ var es = new ElasticGraph(process.argv[2])
 //Get and search retrieve entity object(s) from in memory cache which in turn is flled from ES as each respective query happens for first time. These are mutable objects. The idea is to let them go through a process of multiple mutations in memory during the migration process. 
 //Once the old tables are processed, (only) the updated rows in cache are flushed to ES.
 const saveAttendanceWithUserWorkshopRelationships = [
-        'iterate over raw_attendances as raw_attendance. Get 500 at a time. Flush every 2 cycles. Wait for 2000 millis', [
-            'search first user where {emailId: *raw_attendance.email} as user. Create if not exists.',
+        'iterate over raw_attendances as raw_attendance. Get 200 at a time. Flush every 2 cycles. Wait for 2000 millis', [
+            'search first user where {emailId: *raw_attendance.email} as user.',
             //'search first state where {name: *raw_attendance.state} as state. Create if not exists.',
             'attendance is {_type: attendance, user._id: *user._id, joinTime: *raw_attendance.joinTime, leaveTime: *raw_attendance.leaveTime, timeInSession: *raw_attendance.timeInSession, dayNumber: 1}',
             'index *attendance'
@@ -19,24 +19,8 @@ const saveAttendanceWithUserWorkshopRelationships = [
 const saveUsers = [
     'iterate over raw_users as raw_user. Get 100 at a time. Flush every 2 cycles. Wait for 1000 millis.', [
         'search first state where {name: *raw_user.InstituteState}. Create if not exists.',
-        /**(ctx) => {
-            return ctx.es.deep.get({_id: "00ff0dfdfb3b31fced571bcc0395d461d3dd23d7", _type: "state"})
-            .then((state) => {
-                return ctx.setImmutable('state', state)
-            })
-        },**/
-        //'get state *state._id as state. Join from read',
-        'display state is initially, *state',
         'user is {_type: user, state._id: *state._id emailId: *raw_user.CandidateEmail, firstName: *raw_user.CandidateName}',
-        //'display user before indexing, *user',
         'index *user',
-        //'link *user with *state as state',
-        'display user after indexing, *user',
-        //'link *user with *state as state',
-        //'get user *user._id as user. Join from read',
-        //'display user after linking, *user',
-        //'get state *state._id as state. Join from read',
-        'display state after linking, *state'
     ]
 ]
 
@@ -56,8 +40,8 @@ if (require.main === module) {
     const ctx = new es.Cache(es, {})
 
     const instructions = [
+        saveUsers,
         saveAttendanceWithUserWorkshopRelationships
-        //saveUsers
     ]
     return executeOneByOne(instructions, ctx)
         .then((res) => {
